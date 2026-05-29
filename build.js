67dd42b1441ffdb1f87ac399fe6bb7e14e98550d
five-plus-one <5plus1@five-plus-one.com>
2026-05-29 20:58:30 +0800

    feat: 新的阅卷平台：乐华

--- build.js
+++ build.js
@@ -99,11 +99,13 @@ const BUILD_CONFIGS = [
             'adapters/jiukexing/adapter.js',
             'adapters/huiyuejuan/selectors.js',
             'adapters/huiyuejuan/adapter.js',
+            'adapters/lehua/selectors.js',
+            'adapters/lehua/adapter.js',
         ],
         header: {
             name: 'AI-Marker-Suite',
             namespace: 'https://aimarking.five-plus-one.com/',
-            description: 'AI自动批改助手，支持智学网、七天网络、好分数、五岳阅卷、华翰云、光大阅卷、云阅卷、新教育、鑫考、润建、54学霸、九科星、慧阅卷等平台。自动识别答案、智能评分、自动提交！',
+            description: 'AI自动批改助手，支持智学网、七天网络、好分数、五岳阅卷、华翰云、光大阅卷、云阅卷、新教育、鑫考、润建、54学霸、九科星、慧阅卷、乐华阅卷等平台。自动识别答案、智能评分、自动提交！',
             author: '5plus1',
             match: [
                 'https://www.zhixue.com/*',
@@ -125,6 +127,7 @@ const BUILD_CONFIGS = [
                 '*://54xueba.cn/*',
                 '*://marking.jkxjxw.com/*',
                 '*://web.17yuejuan.cn/*',
+                '*://main.lhsvr.cn/*',
             ],
             include: [
                 '/^https?:\/\/\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+\\//',  // IP:端口 部署（光大阅卷等）
